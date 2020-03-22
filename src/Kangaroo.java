458e62006d24b9a324e0664c5607906ea137d7d1
adnanmerrakchi <adnanmerrakchi@gmail.com>
2020-03-22 16:18:17 +0100

    Solution for Kangaroo problem in hackerrank=> Problem Solving using math.

---
+++ src/Kangaroo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Kangaroo {
+
+    public static void main (String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+
+        if (v1 <= v2) System.out.println("NO");
+        else if( (x2-x1)%(v1-v2) == 0 ) System.out.println("YES");
+        else System.out.println("NO");
+
+    }
+
+}
